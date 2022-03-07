6a8db7dc230fa7dfec3bd8736d76b011ca034876
Chunwol <60349340+Chunwol@users.noreply.github.com>
2022-03-07 10:01:23 +0900

    Update env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -7,8 +7,8 @@ export const dbPort : number = Number(process.env.DB_PORT)! || 3306;
 
 export const dbDatabase : string = process.env.DB_NAME! || 'Mysql';
 
-export const dbUser : string = process.env.DB_USER! || 'hansei';
-export const dbPassword : string = process.env.DB_PW! || 'hansei';
+export const dbUser : string = process.env.DB_USER! || 'null';
+export const dbPassword : string = process.env.DB_PW! || 'null';
 
 export const DiscordToken : string = process.env.DISCORD_TOKEN! || '0';
 
